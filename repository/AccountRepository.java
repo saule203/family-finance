8953c7130cd25ddf095d079e21ce86d2ff02a427
CatFreeFin <154658972+saule203@users.noreply.github.com>
2025-09-24 23:47:37 +0800

    Delete repository directory

--- repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.familyfinance.repository;
-
-import com.example.familyfinance.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account,Long> {
-}
--- repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.familyfinance.repository;
-
-import com.example.familyfinance.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer>{
-    User findByUsername(String name);
-}
